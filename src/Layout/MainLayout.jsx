5ac9d6b035a07c05931a80a9d1bf98d9ba67aafa
Rafi Siddique <rafisiddique67@gmail.com>
2025-01-25 19:45:20 +0600

    initial commit

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,12 @@
+
+const MainLayout = () => {
+    return (
+        <div className='min-h-screen flex flex-col'>
+            <header></header>
+            <main></main>
+            <footer></footer>
+        </div>
+    );
+};
+
+export default MainLayout;
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import AuthProvider from './Provider/AuthProvider.jsx'
+import { RouterProvider } from 'react-router'
+import router from './routes/router.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router}/>
+    </AuthProvider>
+  </StrictMode>,
+)
