1aebffe8eef6155603e60fd5b734dabc535be81a
Rafi Siddique <rafisiddique67@gmail.com>
2025-01-26 01:16:15 +0600

    banner component added

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router";
-import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
+import Header from "../Components/Header";
 
 const MainLayout = () => {
     return (
         <div className='container min-h-screen flex flex-col space-y-2 justify-between mx-auto'>
             <header>
-                <Navbar></Navbar>
+                <Header></Header>
             </header>
             <main className="mx-auto">
                 <Outlet/>
